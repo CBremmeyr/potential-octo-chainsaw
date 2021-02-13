28e6198f1fca13622ff5320a117091f558347d27
Corbin Bremmeyr <bremmeyr@mail.gvsu.edu>
2021-02-13 14:18:02 -0500

    Debugging linear token passing

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 #define MAX_STR_LEN 256
 
 typedef struct{
-    int dest;
+    pid_t dest;
     char data[MAX_STR_LEN];
 } token_t;
 
@@ -37,6 +37,8 @@ int main(int argc, char* args[]){
     int ret = 0;
     int numComps = 0;
     pid_t *pids = NULL;
+    pid_t ogParent = getpid();
+    token_t token = {0};
 
     // Check if user gave at least one comand line argument
     if(argc < 2) {
@@ -63,6 +65,35 @@ int main(int argc, char* args[]){
     pids = malloc(numComps * sizeof(pid_t));
     ret = createNetwork(numComps, pids, fd);
 
+    // UI process
+    if(ogParent == getpid()) {
+        printf("og parent\n");
+        token.dest = 3;
+        strcpy(token.data, "hello there");
+        passToken(fd[WRITE], &token);
+        while(1);
+    }
+    else {
+
+        while(1) {
+
+            // Wait to receive token
+            while(getToken(fd[READ], &token) != 0);
+
+            // Process token
+            if(token.dest == getpid()) {
+                printf("");
+            }
+
+            // Give token to next node
+            if(passToken(fd[WRITE], &token) == 0) {
+                printf("PID = %d : passToken() successful\n", getpid());
+            }
+            else {
+                printf("PID = %d : passToken() failded\n", getpid());
+            }
+        }
+    }
 
     free(pids);
 
@@ -92,17 +123,12 @@ int createNetwork(int n, pid_t *pids, int *fd) {
     return 0;
 }
 
-/************************************************************
- * Creates a processes that represents a new computer recursively
- * A value of n <= 1 will result in no additional processes being created.
- *
- ***********************************************************/
 int createNetworkHelper(int n, pid_t *pids, int *fd) {
 
     pid_t curPid;
     pids[0] = getpid();
 
-    printf("Computer %d: %d\n", n, pids[0]);
+    printf("Computer %d:PID = %d\n", n, pids[0]);
 
     if(n-1 > 0) {
 
@@ -123,9 +149,9 @@ int createNetworkHelper(int n, pid_t *pids, int *fd) {
 
             // Close unused file descriptors
             close(fd[WRITE]);
-            close(stdin);
+            close(STDIN_FILENO);
 
-            return createNetwork(n-1, &pids[1]);
+            return createNetworkHelper(n-1, &pids[1], fd);
         }
 
         // Parent process
@@ -142,6 +168,9 @@ int createNetworkHelper(int n, pid_t *pids, int *fd) {
 /**
  * Read token from pipe (blocking)
  *
+ * param readFd - pipe file descriptor to read from
+ * param *token - location to store token recived from pipe
+ *
  * return 0 on success, -1 if not all data was passed
  */
 int getToken(int readFd, token_t *token) {
@@ -153,13 +182,17 @@ int getToken(int readFd, token_t *token) {
     }
 
     // Print to track token's path
-    printf("PID = %d : recived token\n", getpid());
+    printf("PID = %d : getting token\t", getpid());
+    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
 
     return 0;
 }
 
 /**
  * Pass token to next node in ring
+ *
+ * param writeFd - pipe file descriptor to read from
+ * param *token - location of token to write to pipe
  *
  * return 0 on success, -1 if not all data was passed
  */
@@ -168,7 +201,8 @@ int passToken(int writeFd, token_t *token) {
     sleep(1);
 
     // Print to track token's path
-    printf("PID = %d : passing token\n", getpid());
+    printf("PID = %d : passing token\t", getpid());
+    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
 
     // Write to pipe
     size_t s = write(writeFd, (const void *) token, sizeof(token));
