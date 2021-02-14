545f94af2522817fd0393ddc2a43da6a1891a8b7
Corbin Bremmeyr <bremmeyr@mail.gvsu.edu>
2021-02-13 19:07:25 -0500

    Merged user input & token processing

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
  *
  * Authors: Corbin Bremmeyr
  *          Richard Critchlow
+ * Date: 13 February 2021
  *
  * Entry point for CIS 452 project 1
  */
@@ -14,6 +15,8 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 #define READ 0
 #define WRITE 1
@@ -26,13 +29,27 @@ typedef struct{
     char data[MAX_STR_LEN];
 } token_t;
 
+typedef struct {
+    int nodeCount;
+    int dest;
+    char str[MAX_STR_LEN];
+} userInput_t;
+
+enum TKN_STAT{EMPTY, FULL, RETURNING};
+
 int createNetwork(int len, pid_t *pids, int **fd);
 int spawn(int len, pid_t *pids);
 void fixPipeLeaks(pid_t *pids, int **fd, int len);
 
+enum TKN_STAT checkToken(token_t *token);
 int getToken(int readFd, token_t *token);
+int getTokenNB(int readFd, token_t *token);
 int passToken(int writeFd, token_t *token);
 int pidToIndex(pid_t *pids, int len);
+
+void *getUserInput(void *arg);
+
+volatile bool userInputDone;
 
 int main(int argc, char* args[]){
 
@@ -93,14 +110,75 @@ int main(int argc, char* args[]){
         token.dest = 3;
         strcpy(token.data, "hello there pdidy, how you doin' ;)");
 
+        const int pidIndex = 0;
+        const int writeIndex = pidIndex;
+        const int readIndex = numComps-1;
+        bool threadSpawned = false;
+        userInputDone = true;
+
+        pthread_t thread;
+        int threadStatus;
+        userInput_t userInput = {0};
+        userInput.nodeCount = numComps;
+        userInput.dest = -1;
+
+        bool haveData = false;
+        bool waitForReply = false;
+
         while(1) {
-            const int pidIndex = 0;
-            const int writeIndex = pidIndex;
-            const int readIndex = numComps-1;
 
-            passToken(fd[writeIndex][WRITE], &token);
+            // Try to get token
+            // If token was recived
+            if(getTokenNB(fd[readIndex][READ], &token) == 0) {
 
-            while(getToken(fd[readIndex][READ], &token) != 0);
+                switch(checkToken(&token)){
+                    case RETURNING:
+                        if(waitForReply) {
+                            strcpy(token.data, "");
+                        }
+                        break;
+
+                    case FULL:
+                        if(token.dest == pids[pidIndex]){
+                            printf("PID = %d : This is for me\n\t\"%s\"\n", pids[pidIndex], token.data);
+                            token.dest = 0;
+                        }
+                        break;
+
+                    case EMPTY:
+                        if(haveData) {
+                            token.dest = userInput.dest;
+                            strcpy(token.data, userInput.str);
+                            waitForReply = true;
+                        }
+                        break;
+                }
+                passToken(fd[writeIndex][WRITE], &token);
+            }
+
+            // If user input is needed
+            if(!haveData) {
+
+                // If a user input thread has been created
+                if(threadSpawned) {
+
+                    // If thread has finsihed
+                    if(userInputDone) {
+                        pthread_join(thread, NULL);
+                        threadSpawned = false;
+                    }
+                }
+                else {
+
+                    // Spawn user input thread
+                    if((threadStatus = pthread_create(&thread, NULL, getUserInput, (void *) &userInput)) != 0) {
+                        fprintf(stderr, "Thread create error %d: %s\n", threadStatus, strerror(threadStatus));
+                        // TODO exit maybe?
+                    }
+                    threadSpawned = true;
+                }
+            }
+
         }
     }
 
@@ -132,6 +210,38 @@ int main(int argc, char* args[]){
     free(fd);
 
     return 0;
+}
+
+
+void *getUserInput(void *arg) {
+    userInput_t userInput = *((userInput_t *) arg);
+    char tempStr[MAX_STR_LEN] = {0};
+
+    // Prompt user for node to send message to
+    do {
+        printf("Enter destination node (0 to %d): ", userInput.nodeCount);
+        fgets(tempStr, MAX_STR_LEN, stdin);
+        userInput.dest = atoi(tempStr);
+    } while(userInput.dest < userInput.nodeCount);
+
+    // Promp user for messsage to send
+    printf("Enter message to send (max %d chars):\n", MAX_STR_LEN);
+    fgets(userInput.str, MAX_STR_LEN, stdin);
+
+    // Set falg to show thread has finished
+    userInputDone = true;
+
+    return NULL;
+}
+
+enum TKN_STAT checkToken(token_t *token){
+    if(token->dest == 0) {
+        if(strlen(token->data) == 0) {
+            return EMPTY;
+        }
+        return RETURNING;
+    }
+    return FULL;
 }
 
 int pidToIndex(pid_t *pids, int len) {
@@ -185,9 +295,53 @@ int getToken(int readFd, token_t *token) {
     }
 
     // Print to track token's path
-    printf("PID = %d : getting token\t", getpid());
-    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
+//    printf("PID = %d : getting token\t", getpid());
+//    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
 
+    return 0;
+}
+
+/**
+ * Read token from pipe (non-blocking)
+ *
+ * param readFd - pipe file descriptor to read from
+ * param *token - location to store token recived from pipe
+ *
+ * return 0 on success, -1 if no data was read
+ */
+int getTokenNB(int readFd, token_t *token) {
+
+    // Save original flags
+    int flags = fcntl(readFd, F_GETFL, 0);
+
+    // Set pipe reading to be non-blocking
+    fcntl(readFd, F_SETFL, flags | O_NONBLOCK);
+
+    // Read from pipe
+    ssize_t s = read(readFd, (void *) token, sizeof(token_t));
+
+    // If no data was read
+    if(s == 0) {
+
+        // Restore original pipe flags
+        fcntl(readFd, F_SETFL, flags);
+        return -1;
+    }
+
+    // If only some of the data was read
+    else if(s < sizeof(token_t)) {
+
+        // Restore original pipe flags
+        fcntl(readFd, F_SETFL, flags);
+
+        // Read rest of data
+        char *temp = ((char *) token) + s;
+        read(readFd, (void *) temp, sizeof(token_t)-s);
+        return 0;
+    }
+
+    // Restore original pipe flags
+    fcntl(readFd, F_SETFL, flags);
     return 0;
 }
 
@@ -204,8 +358,8 @@ int passToken(int writeFd, token_t *token) {
     sleep(1);
 
     // Print to track token's path
-    printf("PID = %d : passing token\t", getpid());
-    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
+//    printf("PID = %d : passing token\t", getpid());
+//    printf("Token.dest = %d, .data = %s\n", token->dest, token->data);
 
     // Write to pipe
     size_t s = write(writeFd, (const void *) token, sizeof(token_t));
@@ -257,13 +411,11 @@ void fixPipeLeaks(pid_t *pids, int **fd, int len) {
 
                 // Close read side
                 if(j != readIndex) {
-                    printf("i = %d: close read %d\n", i, j);
                     close(fd[j][READ]);
                 }
 
                 // Close write side
                 if(j != i) {
-                    printf("i = %d: close write %d\n", i, j);
                     close(fd[j][WRITE]);
                 }
             }
